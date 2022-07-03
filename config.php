1e47f65e09319e527ea028a077bc1bd969b3428b
Victor Ferreira Aragon <victor.aragon01@gmail.com>
2022-07-03 19:19:50 -0300

    Adicionado arquivos no projeto
    Via Upload

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+require 'environment.php';
+
+global $config;
+global $db;
+
+$config = array();
+if(ENVIRONMENT == 'development') {
+	define("BASE_URL", "http://localhost/www/LojaCartas2/");
+	$config['dbname'] = 'mundotcg';
+	$config['host'] = 'localhost';
+	$config['dbuser'] = 'root';
+	$config['dbpass'] = '';
+} else {
+	define("BASE_URL", "http://localhost/www/LojaCartas2/");
+	$config['dbname'] = 'mundotcg';
+	$config['host'] = 'localhost';
+	$config['dbuser'] = 'root';
+	$config['dbpass'] = '';
+}
+
+$config["defaultLanguage"] = 'en';
+
+$db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
